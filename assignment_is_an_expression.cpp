a12caa697b32bc2c74c4a437138b0f7ff9083847
Matt Pellegrini <pelleg@amazon.com>
2017-07-26 16:58:05 +0200

    Some interesting (mis)applications of assignments being expressions

--- assignment_is_an_expression.cpp
+++ assignment_is_an_expression.cpp
@@ -1,7 +1,26 @@
+/*
+ * Outputs:
+ * 	5
+ * 	FALSE
+ * 	TRUE
+ * 	6
+ *
+*/
+
 #include <iostream>
+using namespace std;
 
 int main()
 {
-  int x;
-  std::cout << (x = 5) << std::endl;
+  int x = 5;
+
+  cout << "X :" << x << endl;
+  x == 6 ? cout << "TRUE" << endl : cout << "FALSE" << endl;
+  cout << "X :" << x << endl;
+  // ? binds tighter than =, == binds tigher than both.
+  // http://en.cppreference.com/w/c/language/operator_precedence 
+  (x = 6) ? cout << "TRUE" << endl : cout << "FALSE" << endl;
+  cout << "X :" << x << endl;
+  (x = 0) ? cout << "TRUE" << endl : cout << "FALSE" << endl;
+  cout << "X :" << x << endl;
 }
