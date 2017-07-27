4cd1013021b03fa5aa69bbb65cea58c61f461db3
Matt Pellegrini <pelleg@amazon.com>
2017-07-27 16:43:34 +0200

    Update templated functions and let the compiler figure out the types

--- templated_functions.cpp
+++ templated_functions.cpp
@@ -18,10 +18,16 @@ int main()
   cout << "d: " << d << endl;
   cout << "f: " << f << endl;
   cout << "s: " << s << endl;
-  cout << "double_me(x): " << double_me<int>(x) << endl;
-  cout << "double_me(d): " << double_me<double>(d) << endl;
-  cout << "double_me(f): " << double_me<float>(f) << endl;
-  cout << "double_me(s): " << double_me<string>(s) << endl;
+  cout << "double_me<int>(x): " << double_me<int>(x) << endl;
+  cout << "double_me<double>(d): " << double_me<double>(d) << endl;
+  cout << "double_me<float>(f): " << double_me<float>(f) << endl;
+  cout << "double_me<string>(s): " << double_me<string>(s) << endl;
+
+  // In these cases the compiler can deduce the types and therefore function to call automatically, so we can be less explicit.
+  cout << "double_me(x): " << double_me(x) << endl;
+  cout << "double_me(d): " << double_me(d) << endl;
+  cout << "double_me(f): " << double_me(f) << endl;
+  cout << "double_me(s): " << double_me(s) << endl;
 
 }
 
